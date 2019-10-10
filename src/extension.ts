5d810bb8413a060d1f2713ed6e0dfbaa45c50a6b
Sladyn <gunnerforlife00@gmail.com>
2019-10-11 00:31:59 +0530

    Added command and plugin keyword

--- src/extension.ts
+++ src/extension.ts
@@ -13,11 +13,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.helloWorld', () => {
+	let disposable = vscode.commands.registerCommand('extension.jcasc', () => {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
+		vscode.window.showInformationMessage('Welcome to the jcasc-plugin');
 	});
 
 	context.subscriptions.push(disposable);
