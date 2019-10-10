8c80d773ff30c226b5d5a94e718edadcc562459d
Sladyn <gunnerforlife00@gmail.com>
2019-10-11 02:24:36 +0530

    Fetching JSON

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Http2ServerRequest, Http2ServerResponse } from 'http2';
+import { runInThisContext } from 'vm';
+import { getLatestInsidersMetadata } from 'vscode-test/out/util';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -18,6 +21,11 @@ export function activate(context: vscode.ExtensionContext) {
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Welcome to the jcasc-plugin');
+
+		//Fetches the JSON Schema via a REST API Call
+		const endpoint = 'http://localhost:8080/jenkins/configuration-as-code/schema';
+		let resonse = this.get(endpoint);
+		console.log(JSON.stringify(resonse));
 	});
 
 	context.subscriptions.push(disposable);
