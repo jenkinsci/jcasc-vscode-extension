e283b7d547f12428dbb9e64eab1094ae4622a600
Joseph Petersen <josephp90@gmail.com>
2019-11-13 07:24:39 +0100

    prettier formatting

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
-# JCasC-VSCode-Plugin 
-
+# JCasC-VSCode-Plugin
 
 [![Total alerts](https://img.shields.io/lgtm/alerts/g/sladyn98/JCasC-VSCode-Plugin.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/sladyn98/JCasC-VSCode-Plugin/alerts/)
 [![Build Status](https://dev.azure.com/jcasc-vscode-extension/jcasc-vscode-extension/_apis/build/status/jenkinsci.jcasc-vscode-extension?branchName=master)](https://dev.azure.com/jcasc-vscode-extension/jcasc-vscode-extension/_build/latest?definitionId=1&branchName=master)
-
 
 This plugin deals with the authentication and fetching of the JSON schema from a live Jenkins instance.
 It provides the base for validation of yaml files written to configure a Jenkins instance.
 
 ## How to Run Locally
 
-1) Clone the repository.
+1. Clone the repository.
 
-2) Run `npm install` in the working directory.
+2. Run `npm install` in the working directory.
 
-3) Hit `F5` to run the plugin in the development Host.
+3. Hit `F5` to run the plugin in the development Host.
--- src/extension.ts
+++ src/extension.ts
@@ -2,49 +2,48 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-const rp = require("request-promise");
-var fs = require("fs");
+const rp = require('request-promise');
+var fs = require('fs');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  // Use the console to output diagnostic information (console.log) and errors (console.error)
+  // This line of code will only be executed once when your extension is activated
+  console.log('Congratulations, your extension "jcasc-plugin" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "jcasc-plugin" is now active!');
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand('extension.jcasc', () => {
+    // The code you place here will be executed every time your command is executed
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('extension.jcasc', () => {
-		// The code you place here will be executed every time your command is executed
+    // Display a message box to the user
+    vscode.window.showInformationMessage('Welcome to the jcasc-plugin');
+    const schemaURL = vscode.workspace.getConfiguration().get('jcasc.schemaURL');
+    const userName = vscode.workspace.getConfiguration().get('jcasc.userName');
+    const userToken = vscode.workspace.getConfiguration().get('jcasc.userToken');
+    const url = `http://${userName}:${userToken}@${schemaURL}`;
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Welcome to the jcasc-plugin');
-		const schemaURL = vscode.workspace.getConfiguration().get('jcasc.schemaURL');
-		const userName = vscode.workspace.getConfiguration().get('jcasc.userName');
-		const userToken = vscode.workspace.getConfiguration().get('jcasc.userToken');
-		const url = `http://${userName}:${userToken}@${schemaURL}`;
-		
-		// Fetches the JSON Schema via a REST API Call
-		rp(url).then((result:any) => {
-			console.log("Result: " + result);
-			var fileContent = result;
-			fs.writeFile("jcasc-schema.json", fileContent, (err:any) => {
-				if (err) {
-					console.error(err);
-					return;
-				}
-				console.log("File has been created");
-			});
+    // Fetches the JSON Schema via a REST API Call
+    rp(url)
+      .then((result: any) => {
+        console.log('Result: ' + result);
+        var fileContent = result;
+        fs.writeFile('jcasc-schema.json', fileContent, (err: any) => {
+          if (err) {
+            console.error(err);
+            return;
+          }
+          console.log('File has been created');
+        });
+      })
+      .catch((err: any) => {
+        console.log('ERROR: ' + err);
+      });
+  });
 
-		}).catch((err:any) => {
-			console.log("ERROR: " + err);
-		});
-
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
