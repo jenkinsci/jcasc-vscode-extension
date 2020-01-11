fc1cff6f713d15ce0199ee6401e66bca2891c27b
Sladyn <gunnerforlife00@gmail.com>
2020-01-11 15:07:50 +0530

    Added empty credentials handling

--- src/extension.ts
+++ src/extension.ts
@@ -22,10 +22,15 @@ export function activate(context: vscode.ExtensionContext) {
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Welcome to the jcasc-plugin');
-		const schemaURL = vscode.workspace.getConfiguration().get('jcasc.schemaURL');
-		const userName = vscode.workspace.getConfiguration().get('jcasc.userName');
-		const userToken = vscode.workspace.getConfiguration().get('jcasc.userToken');
+		var schemaURL = vscode.workspace.getConfiguration().get('jcasc.schemaURL');
+		var userName = vscode.workspace.getConfiguration().get('jcasc.userName');
+		var userToken = vscode.workspace.getConfiguration().get('jcasc.userToken');
 		const url = `http://${userName}:${userToken}@${schemaURL}`;
+
+
+		if(!schemaURL ) {
+			schemaURL = vscode.window.showInputBox({ prompt: 'Kindly provide a schema URL' });
+		}
 		
 		// Fetches the JSON Schema via a REST API Call
 		rp(url).then((result:any) => {
@@ -39,7 +44,7 @@ export function activate(context: vscode.ExtensionContext) {
 				console.log("File has been created");
 			});
 
-		}).catch((err) => {
+		}).catch((err:any) => {
 			console.log("ERROR: " + err);
 			if(err === ECONNREFUSED) {
 				vscode.window.showErrorMessage('Incorrect URL Or token');
