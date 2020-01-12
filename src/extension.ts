bc7c73eb1dcb5fbda6b91b794caefe849d5a74a0
Joseph Petersen <josephp90@gmail.com>
2020-01-12 23:27:29 +0100

    Use webpack and node-fetch to reduce package size (#23)
    * add bundling
    
    * reduce package size by using node-fetch
    
    * output to workspace folder
    
    * configuration fields should be of type string
    
    * fix webpack watch task
    
    * use lint-staged beta

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,11 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
-
-const rp = require('request-promise')
-var fs = require('fs')
+import fetch, { RequestInit } from 'node-fetch'
+import { promisify } from 'util'
+import { pipeline } from 'stream'
+import { createWriteStream } from 'fs'
+import { join } from 'path'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -15,28 +17,30 @@ export function activate(context: vscode.ExtensionContext) {
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand('extension.jcasc', () => {
+  let disposable = vscode.commands.registerCommand('extension.jcasc', async () => {
+    const folder = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath
+    if (!folder) return
+
+    const streamPipeline = promisify(pipeline)
     // The code you place here will be executed every time your command is executed
 
     // Display a message box to the user
     vscode.window.showInformationMessage('Welcome to the jcasc-plugin')
-    const schemaURL = vscode.workspace.getConfiguration().get('jcasc.schemaURL')
-    const userName = vscode.workspace.getConfiguration().get('jcasc.userName')
-    const userToken = vscode.workspace.getConfiguration().get('jcasc.userToken')
-    const url = `http://${userName}:${userToken}@${schemaURL}`
+    const schemaURL = vscode.workspace.getConfiguration().get('jcasc.schemaURL', '')
+    const userName = vscode.workspace.getConfiguration().get('jcasc.userName', '')
+    const userToken = vscode.workspace.getConfiguration().get('jcasc.userToken', '')
 
     // Fetches the JSON Schema via a REST API Call
-    rp(url)
-      .then((result: any) => {
-        console.log('Result: ' + result)
-        var fileContent = result
-        fs.writeFile('jcasc-schema.json', fileContent, (err: any) => {
-          if (err) {
-            console.error(err)
-            return
-          }
-          console.log('File has been created')
-        })
+    const auth = Buffer.from(`${userName}:${userToken}`).toString('base64')
+    const options: RequestInit = {
+      headers: {
+        Authorization: `Basic ${auth}`,
+      },
+    }
+    await fetch(schemaURL, options)
+      .then(res => {
+        if (!res.ok) throw new Error(`unexpected response ${res.statusText}`)
+        return streamPipeline(res.body, createWriteStream(join(folder, 'jcasc-schema.json')))
       })
       .catch((err: any) => {
         console.log('ERROR: ' + err)
