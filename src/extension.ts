5d640a37ec5b064ae6525c69c2ac88e4e5ed314d
EtienneG <guenyot.etienne@gmail.com>
2021-10-06 01:12:43 +0200

    Hacktoberfest_21_show_output_feedback

--- src/extension.ts
+++ src/extension.ts
@@ -14,6 +14,8 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   const outputChannel = vscode.window.createOutputChannel('JCasC')
+  outputChannel.appendLine('Welcome to the JCasC Plugin')
+  outputChannel.show(true)
 
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
